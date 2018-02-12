0870ceb6783b4c97fd97a3975071ad2731255cff
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-12 12:39:33 -0600

    MAINT: use tile_h/tile_v

--- giraffe/landsat.py
+++ giraffe/landsat.py
@@ -2,8 +2,8 @@
 
 import re
 
-regex = re.compile(r'(?P<sensor>^L[CETO]\d{2})_(?P<region>\w{2})_(?P<tileid>(?P<h>\d{3})'
-                    '(?P<v>\d{3}))_(?P<date_acquired>\d{8})_(?P<date_modified>\d{8})_'
+regex = re.compile(r'(?P<sensor>^L[CETO]\d{2})_(?P<region>\w{2})_(?P<tileid>(?P<tile_h>\d{3})'
+                    '(?P<tile_v>\d{3}))_(?P<date_acquired>\d{8})_(?P<date_modified>\d{8})_'
                     '(?P<collection>C\d{2})_(?P<version>V\d{2})(?P<layer>.*)$')
 
 def info(product_id='LC08_CU_027009_20130701_20170729_C01_V01_SRB4'):
