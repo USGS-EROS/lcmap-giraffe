7c2d8649f4889de2b05b0ef279bfcb140f0bceca
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-12 12:42:07 -0600

    MAINT: strip file extension for layer

--- giraffe/landsat.py
+++ giraffe/landsat.py
@@ -1,11 +1,12 @@
 """ Landsat Product Name to Metadata """
 
 import re
+import os
 
 regex = re.compile(r'(?P<sensor>^L[CETO]\d{2})_(?P<region>\w{2})_(?P<tileid>(?P<tile_h>\d{3})'
                     '(?P<tile_v>\d{3}))_(?P<date_acquired>\d{8})_(?P<date_modified>\d{8})_'
                     '(?P<collection>C\d{2})_(?P<version>V\d{2})(?P<layer>.*)$')
 
 def info(product_id='LC08_CU_027009_20130701_20170729_C01_V01_SRB4'):
-    return regex.match(product_id).groupdict()
+    return regex.match(os.path.splitext(product_id)[0]).groupdict()
 
