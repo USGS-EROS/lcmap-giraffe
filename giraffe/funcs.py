6c65044e8afcbca290e1d1f187c4330ac962f9c2
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-12 11:44:56 -0600

    ENH: unpack elasticsearch doc entry

--- giraffe/funcs.py
+++ giraffe/funcs.py
@@ -15,3 +15,8 @@ def parse_date(item, strptimes):
 def timestamp(series):
     now = datetime.datetime.utcnow()
     return [dict(item, **{"@timestamp": now}) for item in series]
+
+
+def unpack(result):
+    return clean(dict(result, **result['_source']),
+                 {k:k for k in list(result['_source'].keys()) + ['_id']})
