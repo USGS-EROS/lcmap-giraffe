728f652ffcdd9fe81546b7b56fa9c0d73d055ccf
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-14 16:39:52 -0600

    MAINT: pass host into m2m interfaces

--- giraffe/m2m.py
+++ giraffe/m2m.py
@@ -5,27 +5,26 @@ from functools import reduce
 import requests
 
 
-HOST = 'https://earthexplorer.usgs.gov/inventory/json/v/1.4.1'
-def _api(endpoint, data=None):
-    url = '{}/{}'.format(HOST, endpoint)
+def _api(host, endpoint, data=None):
+    url = '{}/{}'.format(host, endpoint)
     body = {'jsonRequest': json.dumps(data)} if data else {}
     return requests.post(url, data=body).json().get('data')
 
 
-def login(username='username', password='password'):
-    return _api('login',
+def login(host, username='username', password='password'):
+    return _api(host, 'login',
                 {'username': username, 'password': password})
 
 
-def search(token, dataset='ARD_TILE', start=1, limit=5e4):
-    return _api('search',
+def search(host, token, dataset='ARD_TILE', start=1, limit=5e4):
+    return _api(host, 'search',
                 {'apiKey': token, 'datasetName': dataset, "sortOrder": "ASC",
                  'startingNumber': start, 'maxResults': limit})
 
 
-def psearch(token, dataset='ARD_TILE', chunk=5e3, limit=5e4):
-    limit = limit or search(token, dataset, limit=1).get('totalHits')
+def psearch(host, token, dataset='ARD_TILE', chunk=5e3, limit=5e4):
+    limit = limit or search(host, token, dataset, limit=1).get('totalHits')
     chunk = min(map(int, [limit, chunk]))
     return reduce(add,
-                  [search(token, dataset, start=i+1, limit=chunk).get('results')
+                  [search(host, token, dataset, start=i+1, limit=chunk).get('results')
                    for i in range(0, limit, chunk)])
