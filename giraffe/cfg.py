7c3484a1f221039c84b85a4227a088e2d9a0f0db
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-14 16:38:37 -0600

    ENH: configs from environment or file

---
+++ giraffe/cfg.py
@@ -0,0 +1,29 @@
+""" Environment variables override default configurations from file """
+
+import os
+import configparser
+
+
+def from_env(section):
+    prefix = 'GIRAFFE_{}_'.format(section.upper())
+    return {k.split(prefix)[1]:v
+            for k, v in os.environ.items() if k.startswith(prefix)}
+
+
+def cfgfile(config_file=None):
+    return config_file or os.getenv('GIRAFFE_CONFIG_PATH',
+                                    os.path.join(os.path.expanduser('~/.usgs'),
+                                                 '.giraffe.ini'))
+
+
+def from_file(section, config_file=None):
+    if not os.path.exists(cfgfile(config_file)):
+        return {}
+    config = configparser.ConfigParser()
+    config.read(cfgfile(config_file))
+    return {opt.upper(): config.get(section, opt)
+            for opt in config.options(section)}
+
+
+def get(section, config_file=None):
+    return dict(from_file(section, config_file), **from_env(section))
