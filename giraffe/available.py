18cda04285d17f8210a601cb72a06b0b6183fe3f
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-14 19:46:29 -0600

    MAINT: make m2m more lazy loading

--- giraffe/available.py
+++ giraffe/available.py
@@ -10,20 +10,23 @@ def metadata(series):
     return [dict(item, **landsat.info(item['_id'])) for item in series]
 
 
-def updates(username, password, chunk, limit, keep):
-    return metadata(map(lambda x: f.clean(keep, x),
-                    m2m.psearch(m2m.login(username, password),
-                                chunk=chunk, limit=limit)))
-
-
-def push(username, password, host, index, chunk=5e3, limit=None):
+def transform(search_result):
     keep_keys = {'acquisitionDate': 'acquisition_date', 'displayId': '_id',
                  'entityId':'entity_id', 'modifiedDate': 'modified_date'}
     strptimes = {'acquisition_date': '%Y-%m-%d',
                  'modified_date': '%Y-%m-%dT%H:%M:%S'}
-    docstore.index(host, index,
-                   f.timestamp(map(partial(f.parse_date, strptimes=strptimes),
-                        updates(username, password, chunk, limit, keep_keys))))
+    return map(partial(f.parse_date, strptimes=strptimes),
+                       metadata(map(lambda x: f.clean(keep_keys, x),
+                                    search_result)))
+
+
+def updates(host, username, password, chunk=5e3, limit=5e4):
+    limit = limit or m2m.slimit(host, m2m.login(host, username, password))
+    chunk = min(map(int, [limit, chunk]))
+    for i in range(0, limit, chunk):
+        yield f.timestamp(transform(
+                    m2m.search(host, m2m.login(host, username, password),
+                               start=i+1, limit=chunk)))
 
 
 def tiles(host, index, **kwargs):
--- giraffe/m2m.py
+++ giraffe/m2m.py
@@ -1,7 +1,5 @@
 """ Client for EarthExplorer MachineToMachine API """
 import json
-from operator import add
-from functools import reduce
 import requests
 
 
@@ -19,12 +17,8 @@ def login(host, username='username', password='password'):
 def search(host, token, dataset='ARD_TILE', start=1, limit=5e4):
     return _api(host, 'search',
                 {'apiKey': token, 'datasetName': dataset, "sortOrder": "ASC",
-                 'startingNumber': start, 'maxResults': limit})
+                 'startingNumber': start, 'maxResults': limit}).get('results')
 
 
-def psearch(host, token, dataset='ARD_TILE', chunk=5e3, limit=5e4):
-    limit = limit or search(host, token, dataset, limit=1).get('totalHits')
-    chunk = min(map(int, [limit, chunk]))
-    return reduce(add,
-                  [search(host, token, dataset, start=i+1, limit=chunk).get('results')
-                   for i in range(0, limit, chunk)])
+def slimit(host, token, dataset='ARD_TILE'):
+    return search(host, token, dataset, limit=1).get('totalHits')
