22712420676497d48a38a982a565a44f3e8fcd93
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-16 16:34:33 -0600

    ENH: m2m search items

--- giraffe/m2m.py
+++ giraffe/m2m.py
@@ -14,11 +14,44 @@ def login(host, username='username', password='password'):
                 {'username': username, 'password': password})
 
 
-def search(host, token, dataset='ARD_TILE', start=1, limit=5e4):
-    return _api(host, 'search',
-                {'apiKey': token, 'datasetName': dataset, "sortOrder": "ASC",
-                 'startingNumber': start, 'maxResults': limit}).get('results')
+def add_additional(dataset='ARD_TILE', **kwargs):
+    fields = {
+        'ARD_TILE': { # 'fieldId' values differ for each M2M dataset
+            'tile_h': 21787,
+            'tile_v': 21788,
+            'region': 21789,
+        }
+    }
+    return {} if len(kwargs) < 1 else {
+        'additionalCriteria': {
+        "filterType": "and",
+            "childFilters": [
+                {"filterType": "value", "fieldId": fields[dataset][k], "value": v}
+                for k,v in kwargs.items()
+            ]
+        }}
 
 
-def slimit(host, token, dataset='ARD_TILE'):
-    return search(host, token, dataset, limit=1).get('totalHits')
+def add_temporal(**kwargs):
+    fields = {'start': 'startDate', 'end': 'endDate'}
+    return {} if len(kwargs) < 1 else {
+        'temporalFilter':
+            dict(dateField='search_date',
+                 **{fields[k]: v for k,v in kwargs.items()})
+        }
+
+
+def build_meta_search(**kwargs):
+    funcs = {'add_': add_additional, 'temporal_': add_temporal}
+    return dict([(key,d[key]) for d in [f(**{k.split(y)[1]: v for k,v in kwargs.items()
+                 if k.startswith(y)}) for y,f in funcs.items()] for key in d])
+
+
+def search(host, token, dataset='ARD_TILE', start=1, limit=5e4, **kwargs):
+    query = {'apiKey': token, 'datasetName': dataset, "sortOrder": "ASC",
+             'startingNumber': start, 'maxResults': limit}
+    return _api(host, 'search', dict(query, **build_meta_search(**kwargs)))
+
+
+def slimit(host, token, dataset='ARD_TILE', **kwargs):
+    return search(host, token, dataset, limit=1, **kwargs).get('totalHits')
