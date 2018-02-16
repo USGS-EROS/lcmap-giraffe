dd44b10f95e06c9a6eb6d6f5019b4f43f4f68abb
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-16 17:03:53 -0600

    MAINT: bruteforce updates to elastic

--- giraffe/cli.py
+++ giraffe/cli.py
@@ -1,6 +1,13 @@
 """ Command-line-interface entrypoint for Docker """
 
 import argparse
+import logging
+from itertools import islice
+
+from . import available
+from . import ingested
+from . import docstore
+from . import cfg
 
 
 def parse_args(defaults=None):
@@ -11,11 +18,28 @@ def parse_args(defaults=None):
     return defaults
 
 
+def configure_log(level='INFO'):
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=level, format=fmt)
+
+
 def run():
-    print('run giraffe run!')
+    data = available.updates(**{k.lower(): v for k, v in cfg.get('m2m').items()})
+    i = 0
+    while True:
+        d = list(islice(data, i, i+1))[0] # Grab a single chunk via HTTP
+        docstore.index(host=cfg.get('ard')['ES_HOST'], index=cfg.get('ard')['ES_INDEX'],
+                       data=d)
+        i+=1
+
+        docstore.index(host=cfg.get('iwds')['ES_HOST'],
+                        index=cfg.get('iwds')['ES_INDEX'],
+                        data=ingested.updates(host=cfg.get('iwds')['URL'],
+                        tiles=d))
 
 
 def main():
+    configure_log()
     globals()[parse_args()['command']]()
 
 
