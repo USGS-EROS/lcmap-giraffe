5c368fe7decf65141612e0eb853ff444cdd993f3
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-20 15:13:59 -0600

    MAINT: use a semaphore to limit concurrency

--- giraffe/asynchttp.py
+++ giraffe/asynchttp.py
@@ -5,15 +5,20 @@ import asyncio
 import aiohttp
 
 
-async def async_fetch(url, data=None, verb='GET', limit_per_host=10):
-    conn = aiohttp.TCPConnector(limit_per_host=limit_per_host)
-    async with aiohttp.ClientSession(connector=conn) as session:
-        async with session.request(verb, url, data=data) as response:
-            return await response.json()
+async def async_fetch(url, data=None, verb='GET'):
+    async with aiohttp.ClientSession() as session:
+            async with session.request(verb, url, data=data) as response:
+                return await response.json()
+
+
+async def sema_fetch(sem, url, data=None, verb='GET'):
+    async with sem:
+        await async_fetch(url, data, verb)
 
 
 def request(reqs, plimit=10):
-    func = partial(async_fetch, limit_per_host=plimit)
+    sem = asyncio.Semaphore(plimit)
+    func = partial(sema_fetch, sem=sem)
     return asyncio.get_event_loop().run_until_complete(
                 asyncio.gather(*(asyncio.ensure_future(
                     func(**r)) for r in reqs)))
