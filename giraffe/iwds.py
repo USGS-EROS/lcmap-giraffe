7969256339ec19d34f2a737c000e083219df7f20
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-20 15:19:30 -0600

    MAINT: use async for iwds queries

--- giraffe/iwds.py
+++ giraffe/iwds.py
@@ -1,13 +1,12 @@
 """ Client for LCMAP Information Warehouse Data Store """
 
-from operator import add
-import requests
+from giraffe import asynchttp
 
 
 def inventory(host, tifs):
-    return [dict(_id=tif, inventory=requests.get(
-                 '{}/inventory?only=source&only=extra&source={}'
-                 .format(host, tif)).json()) for tif in tifs]
+    urls = ('{}/inventory?only=source&only=extra&source={}'
+            .format(host, tif) for tif in tifs)
+    return asynchttp.request(list(map(asynchttp.make_req, urls)))
 
 
 def http_date(response):
