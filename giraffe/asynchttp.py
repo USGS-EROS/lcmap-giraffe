9526e93f461ef550f4caca31614450fd8bd0d4ac
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-20 18:21:41 -0600

    BUG: fix semaphore madness

--- giraffe/asynchttp.py
+++ giraffe/asynchttp.py
@@ -5,23 +5,18 @@ import asyncio
 import aiohttp
 
 
-async def async_fetch(url, data=None, verb='GET'):
+async def async_fetch(sem, url, data=None, verb='GET'):
     async with aiohttp.ClientSession() as session:
+        async with sem:
             async with session.request(verb, url, data=data) as response:
                 return await response.json()
 
 
-async def sema_fetch(sem, url, data=None, verb='GET'):
-    async with sem:
-        await async_fetch(url, data, verb)
-
-
 def request(reqs, plimit=10):
-    sem = asyncio.Semaphore(plimit)
-    func = partial(sema_fetch, sem=sem)
+    sema = partial(async_fetch, sem=asyncio.Semaphore(plimit))
     return asyncio.get_event_loop().run_until_complete(
                 asyncio.gather(*(asyncio.ensure_future(
-                    func(**r)) for r in reqs)))
+                    sema(**r)) for r in reqs)))
 
 
 def make_req(url, verb='GET', data=None):
