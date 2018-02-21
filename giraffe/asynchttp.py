7c564b7ddff51fd1a241968e70238462f58307bb
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-20 22:15:53 -0600

    MAINT: fix too many connections opened

--- giraffe/asynchttp.py
+++ giraffe/asynchttp.py
@@ -5,18 +5,24 @@ import asyncio
 import aiohttp
 
 
-async def async_fetch(sem, url, data=None, verb='GET'):
-    async with aiohttp.ClientSession() as session:
-        async with sem:
-            async with session.request(verb, url, data=data) as response:
-                return await response.json()
+async def async_fetch(sem, session, url, data=None, verb='GET'):
+    async with sem:
+        async with session.request(verb, url, data=data) as response:
+            return await response.json()
+
+
+async def gather(reqs, plimit=10):
+    sema = partial(async_fetch, sem=asyncio.Semaphore(plimit))
+    conn = aiohttp.TCPConnector(limit=plimit)
+    async with aiohttp.ClientSession(connector=conn) as session:
+        coroutines = (asyncio.ensure_future(
+                      sema(session=session, **r)) for r in reqs)
+        responses = await asyncio.gather(*coroutines)
+    return responses
 
 
 def request(reqs, plimit=10):
-    sema = partial(async_fetch, sem=asyncio.Semaphore(plimit))
-    return asyncio.get_event_loop().run_until_complete(
-                asyncio.gather(*(asyncio.ensure_future(
-                    sema(**r)) for r in reqs)))
+    return asyncio.get_event_loop().run_until_complete(gather(reqs, plimit))
 
 
 def make_req(url, verb='GET', data=None):
